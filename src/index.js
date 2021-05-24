9db158fcd089846124b235062c61694e40d8de72
Dilsad Mohammed <76966565+dilsadmohammed4@users.noreply.github.com>
2021-05-24 20:17:25 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import * as calculator from "./calculator";
+
+//Import the add, multiply, subtract and divide functions
+//from the calculator.js file.
+//If successful, your website should look the same as the Final.png
+
+ReactDOM.render(
+  <ul>
+    <li>{calculator.add(1, 2)}</li>
+    <li>{calculator.multiply(2, 3)}</li>
+    <li>{calculator.subtract(7, 2)}</li>
+    <li>{calculator.divide(5, 2)}</li>
+  </ul>,
+  document.getElementById("root")
+);
